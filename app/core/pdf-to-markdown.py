6d9c0c51efefbd9875017a1f8b2aea9e4c2f435c
aebenbach <andrewebenbach@gmail.com>
2025-10-11 21:25:40 -0400

    bones

---
+++ app/core/pdf-to-markdown.py
@@ -0,0 +1,19 @@
+import os
+
+class PDFToMarkdown():
+    PDF_BASE: str = 'recipes/markdown'
+    MD_BASE: str = 'recipes/markdown'
+    def __init__(self):
+        pass 
+
+    def _load_pdf(path: str):
+        pass 
+
+    def _extract_txt(pdf_data):
+        pass
+
+    def _save_as_md(text: str, path: str):
+        pass 
+
+    def pdf_to_md(fname: str):
+        pass 
