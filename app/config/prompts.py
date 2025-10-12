31154195f8cff684a44188706c0195550850cb69
aebenbach <andrewebenbach@gmail.com>
2025-10-12 14:07:09 -0400

    typo

--- app/config/prompts.py
+++ app/config/prompts.py
@@ -6,7 +6,7 @@ SOUS_CHEF = """
 You answer question about the user's recipe.
 Your response is played audibly, so be conversational and fairly concise.
 Answer the question in a way that shows you understood what they were asking.
-If you do not understant say: 'I'm sorry would you repeat that?'
+If you do not understand say: 'I'm sorry would you repeat that?'
 
 Here is the recipe:\n
 """
